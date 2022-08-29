ad9473c4ad3391364231c3fe180ac94032bc9646
Davi Silva <davisilvaphoto@gmail.com>
2022-08-29 08:02:05 -0300

    starting to implement Board and Piece

---
+++ src/main/java/boardgame/Piece.java
@@ -0,0 +1,15 @@
+package boardgame;
+
+public class Peace {
+    protected Position position;
+    private Board board;
+
+    public Peace(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
