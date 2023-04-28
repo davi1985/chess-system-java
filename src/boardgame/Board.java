64e340029a5af3a7893a21edf5d13fc35653d0eb
Davi Silva <davisilvaphoto@gmail.com>
2023-04-28 08:45:22 -0300

    chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,29 +1,38 @@
 package boardgame;
 
 public class Board {
-    private int row;
-    private int column;
+    private int rows;
+    private int columns;
     private Piece[][] pieces;
 
     public Board(int row, int column) {
-        this.row = row;
-        this.column = column;
+        this.rows = row;
+        this.columns = column;
         pieces = new Piece[row][column];
     }
 
-    public int getRow() {
-        return row;
+    public int getRows() {
+        return rows;
     }
 
-    public void setRow(int row) {
-        this.row = row;
+    public void setRows(int rows) {
+        this.rows = rows;
     }
 
-    public int getColumn() {
-        return column;
+    public int getColumns() {
+        return columns;
     }
 
-    public void setColumn(int column) {
-        this.column = column;
+    public void setColumn(int columns) {
+        this.columns = columns;
+    }
+
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
